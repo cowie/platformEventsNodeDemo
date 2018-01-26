f751ee6c1891c994d053225b436682aafd7b4825
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2018-01-26 11:53:02 -0800

    oh thats not how any of this works

--- routes/index.js
+++ routes/index.js
@@ -18,35 +18,32 @@ router.post('/upsertRecord/:recordId', function(req, res, next){
   //new, or 
   console.log('upsert detected');
   console.log(req.params);
-  console.log('recordID = ' + req.params.recordId);
-  switch(req.params.recordId){
-    case req.params.recordId.startsWith('001'):
-      console.log('Account');
-      console.log(req.body);
-      /*
-      client.query('INSERT INTO "public"."sfdcAccount"("sfdcID", "name") VALUES($1, $2) RETURNING "id"', [message.payload.ObjectRecordID__c, message.payload.Name__c], (err,res)=>{
+
+  if(req.params.recordId.startsWith('001')){
+    console.log('Account');
+    console.log(req.body);
+    /*
+    client.query('INSERT INTO "public"."sfdcAccount"("sfdcID", "name") VALUES($1, $2) RETURNING "id"', [message.payload.ObjectRecordID__c, message.payload.Name__c], (err,res)=>{
+      if(err) console.error(err);
+    });
+    */
+    } else if (req.params.recordId.startsWith('003')) {
+    console.log('Contact');
+    console.log(req.body);
+    /*
+    if(message.payload.AdditionalData__c != null){
+      var acctID = message.payload.AdditionalData__c.accountID;
+    
+      client.query('INSERT INTO "public"."sfdcContact"(sfdcID, name, accountID) VALUES($1, $2, $3) RETURNING id', [message.payload.ObjectRecordID__c, message.payload.Name__c, acctID], (err,res)=>{
         if(err) console.error(err);
+        
       });
-      */
-      break;
-    case req.params.recordId.startsWith('003'):
-      console.log('Contact');
-      console.log(req.body);
-      /*
-      if(message.payload.AdditionalData__c != null){
-        var acctID = message.payload.AdditionalData__c.accountID;
-      
-        client.query('INSERT INTO "public"."sfdcContact"(sfdcID, name, accountID) VALUES($1, $2, $3) RETURNING id', [message.payload.ObjectRecordID__c, message.payload.Name__c, acctID], (err,res)=>{
-          if(err) console.error(err);
-          
-        });
-      }
-      */
-      break;
-    default:
+    }
+    */
+    } else {
       console.log('Uh nop.');
-  }
-  res.sendStatus(200);
+    }
+    res.sendStatus(200);
 });
 
 
