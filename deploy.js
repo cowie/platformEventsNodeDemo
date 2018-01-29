0965e9416e77e4f3d728c938892171fdef7bc015
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2018-01-29 11:24:28 -0800

    remember that time i messed up the deploy

--- deploy.js
+++ deploy.js
@@ -7,15 +7,17 @@ client.connect();
 
 var acctQuery = 'CREATE TABLE "public"."sfdcAccount" ("id" serial,"name" text,"sfdcID" text,"createdDateTime" text,PRIMARY KEY ("id"))';
 var contQuery = 'CREATE TABLE "public"."sfdcContact" ("id" serial,"name" text,"sfdcID" text,"createdDateTime" text,"accountID" text,PRIMARY KEY ("id"))';
-
+console.log('entering query system');
 client.query(acctQuery, function(err, res){
     if(err){
         console.log(err);
     }else{
+        console.log('account table created');
         client.query(contQuery, function(err,res){
             if(err){
                 console.log(err);
             }else{
+                console.log('contact table created');
                 console.log('db setup completed');
                 client.end();
             }
