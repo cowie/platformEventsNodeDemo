bd202e03f8f10c2936794d2753e3b4d51189ebaa
Chris De Gour <cdegour@cdegour-ltm2.internal.salesforce.com>
2018-01-29 11:14:31 -0800

    added deploy script onto things for funsies, probably need to pipe this to gh dont I

---
+++ deploy.js
@@ -0,0 +1,24 @@
+const pg = require("pg");
+
+const connectionString = process.env.DATABASE_URL;
+const client = new pg.Client(connectionString);
+
+client.connect();
+
+var acctQuery = 'CREATE TABLE "public"."sfdcAccount" ("id" serial,"name" text,"sfdcID" text,"createdDateTime" text,PRIMARY KEY ("id"))';
+var contQuery = 'CREATE TABLE "public"."sfdcContact" ("id" serial,"name" text,"sfdcID" text,"createdDateTime" text,"accountID" text,PRIMARY KEY ("id"))';
+
+client.query(acctQuery, function(err, res){
+    if(err){
+        console.log(err);
+    }else{
+        client.query(contQuery, function(err,res){
+            if(err){
+                console.log(err);
+            }else{
+                console.log('db setup completed');
+            }
+        });
+    }
+})
+
